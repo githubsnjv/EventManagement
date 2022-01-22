10e7c7aaf0e90c1a840a0754f1162c701ae6f993
Sanjv <snjvacoz@gmail.com>
2022-01-22 15:52:28 +0530

    inital

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fireapp';
+  
+  item$: Observable<any[]>;
+  constructor(firestore: AngularFirestore) {
+    this.item$ = firestore.collection('test').valueChanges();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBX_VXu2oOEvRDslQOLEaFaX6Xk2FFYiJs",
+  authDomain: "angular-a5017.firebaseapp.com",
+  projectId: "angular-a5017",
+  storageBucket: "angular-a5017.appspot.com",
+  messagingSenderId: "149922348553",
+  appId: "1:149922348553:web:849a48d0a783392def82d8",
+  measurementId: "G-B274V3GBVD"
+  }
 };
 
 /*
